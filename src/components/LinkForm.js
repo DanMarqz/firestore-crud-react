73495d81997d9e8e73d0fb5feb2a4db6c5cd1393
Daniel <danielarturo.marquez@gmail.com>
2021-05-08 19:14:15 -0400

    fixbug initialStateValues

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -37,7 +37,7 @@ const LinkForm = (props) => {
     } else {
       getLinkById(props.currentId);
     }
-  }, [props.currentId]);
+  }, [initialStateValues, props.currentId]);
 
   return (
     <form className="card card-body" onSubmit={handleSubmit}>
